044f7b889f8cf24937e5106f69a2870b327cc621
Carlos Tellez <carlos.cuervo@threatx.com>
2025-07-14 16:00:54 -0500

    Added storage browser and auth configuration

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,12 @@
+import { defineStorage } from "@aws-amplify/backend";
+  
+export const storage = defineStorage({
+    name: 'vehlo-storage-browser',
+    access: allow => ({
+    'test-mailbox/*': [
+        allow.guest.to(['get']),
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.groups(['read']).to(['read']),
+    ]
+    })
+});
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from '@aws-amplify/ui-react';
+import { StorageBrowser } from '../components/StorageBrowser';
 
 Amplify.configure(outputs);
 
@@ -32,21 +34,41 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator 
+      hideSignUp
+      components={{
+        Header() {
+          return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px' }}>
+              <img
+                src="https://vehlo.com/wp-content/uploads/Vehlo-Logo_RGB_Light.png"
+                alt="Custom Logo"
+                style={{ width: '350px', height: 'auto' }}
+              />
+              <h2 style={{ margin: '10px 0' }}>Welcome to Vehlo S3 Bucket files</h2>
+            </div>
+          );
+        },
+      }}
+    >
+    
+      {({ signOut, user }) => (
+        <main>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px' }}>
+              <img
+                src="https://vehlo.com/wp-content/uploads/Vehlo-Logo_RGB_Light.png"
+                alt="Custom Logo"
+                style={{ width: '350px', height: 'auto' }}
+              />
+              <h2 style={{ margin: '10px 0' }}>Welcome to Vehlo S3 Bucket files</h2>
+            </div>
+            <h1>Hello {user?.signInDetails?.loginId}</h1>
+            <button onClick={signOut}>Sign out</button>
+            {/* StorageBrowser Component */}
+            <h2>Your Files</h2>
+            <StorageBrowser />
+        </main>
+      )}
+    </Authenticator>
   );
 }
